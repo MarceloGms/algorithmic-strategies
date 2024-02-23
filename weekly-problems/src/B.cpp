f0ac1d2620926fe87725f7e7a7313eb71c298151
Marcelo Gomes <mars.gomes2003@gmail.com>
2024-02-23 12:11:13 +0000

    Refactor chessboard function to improve readability and performance

--- weekly-problems/src/B.cpp
+++ weekly-problems/src/B.cpp
@@ -9,22 +9,27 @@ typedef struct knight{
   int nMoves;
 } Knight;
 
-void chessboard(int n, int x, int y, int &count, set<pair<int, int>> &visited) {
-  if (n != 0) {
-    pair<int, int> currentPosition = make_pair(x, y);
-    if (visited.find(currentPosition) == visited.end()) {
-      count++;
-      visited.insert(currentPosition);
-    }
-    chessboard(n-1, x+2, y+1, count, visited);
-    chessboard(n-1, x+2, y-1, count, visited);
-    chessboard(n-1, x+1, y-2, count, visited);
-    chessboard(n-1, x-1, y-2, count, visited);
-    chessboard(n-1, x-2, y-1, count, visited);
-    chessboard(n-1, x-2, y+1, count, visited);
-    chessboard(n-1, x-1, y+2, count, visited);
-    chessboard(n-1, x+1, y+2, count, visited);
+int chessboard(int n, int x, int y, set<pair<int, int>> &visited) {
+  int count = 0;
+
+  pair<int, int> currentPosition = make_pair(x, y);
+  if (visited.find(currentPosition) == visited.end()) {
+    visited.insert(currentPosition);
+    count++;
   }
+
+  if (n > 0) {
+    count += chessboard(n-1, x+2, y+1, visited);
+    count += chessboard(n-1, x+2, y-1, visited);
+    count += chessboard(n-1, x+1, y-2, visited);
+    count += chessboard(n-1, x-1, y-2, visited);
+    count += chessboard(n-1, x-2, y-1, visited);
+    count += chessboard(n-1, x-2, y+1, visited);
+    count += chessboard(n-1, x-1, y+2, visited);
+    count += chessboard(n-1, x+1, y+2, visited);
+  }
+
+  return count;
 }
 
 int main() {
@@ -32,24 +37,18 @@ int main() {
   cin >> n;
   if (n < 1 || n > 30)
     return 1;
-  int count = 0;
-  vector<Knight> knights;
+  int totalCount = 0;
   Knight k;
   set<pair<int, int>> visited;
 
   for(int i = 0; i < n; i++) {
-    cin >> k.posX;
-    cin >> k.posY;
-    cin >> k.nMoves;
+    cin >> k.posX >> k.posY >> k.nMoves;
     if (k.nMoves < 0 || k.nMoves > 7)
       return 1;
     cin.ignore();
-    knights.push_back(k);
+    totalCount += chessboard(k.nMoves, k.posX, k.posY, visited);
   }
 
-  for(auto e : knights)
-    chessboard(e.nMoves, e.posX, e.posY, count, visited);
-
-  cout << count << endl;
+  cout << totalCount << endl;
   return 0;
 }
